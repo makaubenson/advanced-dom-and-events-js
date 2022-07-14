09cd0c0cd26656d915280da671d1959d0ab05e0e
Benson <bensonmakau2000@gmail.com>
2022-07-14 11:28:24 +0300

    fixed modal scroll bug

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
