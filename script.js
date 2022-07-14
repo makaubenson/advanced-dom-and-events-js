0d8d8ccb143685497fd30992d4ede1ffbb945f0c
Benson <bensonmakau2000@gmail.com>
2022-07-14 11:31:14 +0300

    fixed open modal buttons bug

--- script.js
+++ script.js
@@ -19,8 +19,9 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+// for (let i = 0; i < btnsOpenModal.length; i++)
+//   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
