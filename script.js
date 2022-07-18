6297a9559de541108a1ded53840fe0355146f69c
Benson <bensonmakau2000@gmail.com>
2022-07-18 23:20:58 +0300

    Sticky Navigation Menu
    older way of adding sticky menu

--- script.js
+++ script.js
@@ -147,6 +147,16 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 //   handleHover(e, 1);
 // });
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+//Sticky Navigation Menu
+const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
+window.addEventListener('scroll', function () {
+  // console.log(window.scrollY);
+  if (this.window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+});
+
 ////////////////////////////////////////
 // Page Navigation
 ///////////////////////////////////////
